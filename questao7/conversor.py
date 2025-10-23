20c6ce49a75719c8ba2f5063001a224bd7d62277
Guilherme Cavalcante <138344567+Guilhcaval@users.noreply.github.com>
2025-10-23 18:27:07 -0300

    Delete questao7 directory

--- questao7/conversor.py
+++
@@ -1,5 +0,0 @@
-def conversor_valor(valor_produto, juros_produto):
-    conversor_porcentagem = juros_produto / 100
-    total_valor = valor_produto * conversor_porcentagem
-
-    return total_valor
--- questao7/conversor_main.py
+++
@@ -1,8 +0,0 @@
-from  conversor import conversor_valor
-
-valor_produto = float(input("Digite o valor do produto: "))
-juros_produto = float(input("Digite a '%' do produto: " ))
-
-valor = conversor_valor(valor_produto, juros_produto)
-
-print(f"O valor toral da sua compra é: {valor + valor_produto}")
